486560f8ae9b3b2af9c9203fb2cc0aa88147d77e
PJH6029 <top231902@naver.com>
2021-05-07 05:24:22 +0900

    PDE class(w/ discrete poisson equation method) is added

---
+++ PDE.py
@@ -0,0 +1,236 @@
+import numpy as np
+
+
+class PDE:
+    @staticmethod
+    def PoissonEquation(xrange, yrange, h,
+                        uL=None, uR=None, uB=None, uT=None, f=None,
+                        un_axis=None, un=None,  # for mixed boundary condition
+                        boundary_type='Dirichlet'):
+        if not f:  # Laplace Equation
+            def zero_func(x, y):
+                return 0
+            poisson_func = zero_func
+        else:
+            poisson_func = f
+
+        if boundary_type == 'Dirichlet':
+            if uL == None or uR == None or uB == None or uT == None :  # all 쓰고싶은데 경계가 0인거 때문에 안됨 ㅜ
+                raise Exception("insufficient boundary conditions: uL, uR, uB, uT")
+            return PDE.__PoissonEquationWithDirichlet(xrange, yrange, h, uL, uR, uB, uT, poisson_func)
+        elif boundary_type.lower() == 'neumann' or 'mixed':
+            if not all([un_axis, un]):
+                raise Exception("insufficient boundary conditions: un_axis, un")
+            if un_axis == 'x' : un_axis = 0
+            elif un_axis == 'y' : un_axis = 1
+            return PDE.__PoissonEquationWithNeumann(xrange, yrange, h, un_axis, un, poisson_func, uL, uR, uB, uT)
+
+    @staticmethod
+    def __PoissonEquationWithDirichlet(xrange, yrange, h, uL, uR, uB, uT, poisson_func):
+        from Iteration import Iteration
+        x_linspace = np.arange(xrange[0], xrange[1] + h, h)
+        y_linspace = np.arange(yrange[0], yrange[1] + h, h)
+
+        m = len(x_linspace)
+        n = len(y_linspace)
+        vec_len = (m-2) * (n-2)
+
+        if type(uL) == int or float:  # uL is a constant
+            left_boundary_values = np.array([uL for _ in range(n-2)])
+        else:  # uL is a function
+            left_boundary_values = np.array([uL(x_linspace[0], y_linspace[j]) for j in range(1, n-1)])
+
+        if type(uR) == int or type(uR) == float:  # uR is a constant
+            right_boundary_values = np.array([uR for _ in range(n-2)])
+        else:  # uR is a function
+            right_boundary_values = np.array([uR(x_linspace[-1], y_linspace[j]) for j in range(1, n-1)])
+
+        if type(uB) == int or type(uB) == float:  # uB is a constant
+            bottom_boundary_values = np.array([uB for _ in range(m-2)])
+        else:  # uB is a function
+            bottom_boundary_values = np.array([uB(x_linspace[i], y_linspace[0]) for i in range(1, m-1)])
+
+        if type(uT) == int or type(uT) == float:  # uT is a constant
+            top_boundary_values = np.array([uT for _ in range(m - 2)])
+        else:  # uT is a function
+            top_boundary_values = np.array([uT(x_linspace[i], y_linspace[-1]) for i in range(1, m-1)])
+
+        x_grid, y_grid = np.meshgrid(x_linspace[1:-1], y_linspace[1:-1])
+        # m-2 x n-2
+
+        x_coordinates = x_grid.ravel()
+        y_coordinates = y_grid.ravel()
+
+        # 경계보다 한칸 안에 있는 점들 indices
+        left_inner_boundary_indices = np.squeeze(np.where(x_coordinates == x_linspace[1]))
+        right_inner_boundary_indices = np.squeeze(np.where(x_coordinates == x_linspace[m - 2]))
+        bottom_inner_boundary_indices = np.squeeze(np.where(y_coordinates == y_linspace[1]))
+        top_inner_boundary_indices = np.squeeze(np.where(y_coordinates == y_linspace[n - 2]))
+
+        # f(x,y) 넣어줌
+        b_vec = np.array([pow(h, 2) * poisson_func(x_coordinates[i], y_coordinates[i]) for i in range(vec_len)])
+
+        # boundary conditions 넣어줌
+        for j, left_index in enumerate(left_inner_boundary_indices):
+            b_vec[left_index] -= left_boundary_values[j]
+        for j, right_index in enumerate(right_inner_boundary_indices):
+            b_vec[right_index] -= right_boundary_values[j]
+        for i, bottom_index in enumerate(bottom_inner_boundary_indices):
+            b_vec[bottom_index] -= bottom_boundary_values[i]
+        for i, top_index in enumerate(top_inner_boundary_indices):
+            b_vec[top_index] -= top_boundary_values[i]
+
+        D = -4 * np.identity(m-2) + np.eye(m-2, k=-1) + np.eye(m-2, k=1)
+        I = np.identity(m-2)
+
+        A = np.block([[D if i == j else I for i in range(n-2)] for j in range(n-2)])
+        # A * u_vec = b_vec
+
+        u_vec = Iteration.GaussSeidelMethod(coeff_mat=A, b_vec=b_vec)
+
+        return u_vec
+
+    @staticmethod
+    def __PoissonEquationWithNeumann(xrange, yrange, h, un_axis, un, poisson_func, uL=None, uR=None, uB=None, uT=None):
+        if un_axis == 0:
+            return PDE.__NeumannWithAxisX(xrange, yrange, h, un, poisson_func, uL, uR, uB, uT)
+        elif un_axis == 1:
+            return PDE.__NeumannWithAxisY(xrange, yrange, h, un, poisson_func, uL, uR, uB, uT)
+
+    @staticmethod
+    def __NeumannWithAxisX(xrange, yrange, h, un, poisson_func, uL=None, uR=None, uB=None, uT=None):
+        # uR이 없다는 가정하에 작성
+        # un is a function of y
+
+        from Iteration import Iteration
+        x_linspace = np.arange(xrange[0], xrange[1] + h, h)
+        y_linspace = np.arange(yrange[0], yrange[1] + h, h)
+
+        m = len(x_linspace)
+        n = len(y_linspace)
+
+        vec_len = (m - 1) * (n - 2)
+
+        if type(uL) == int or float:  # uL is a constant
+            left_boundary_values = np.array([uL for _ in range(n - 2)])
+        else:  # uL is a function
+            left_boundary_values = np.array([uL(x_linspace[0], y_linspace[j]) for j in range(1, n - 1)])
+
+        if type(uB) == int or type(uB) == float:  # uB is a constant
+            bottom_boundary_values = np.array([uB for _ in range(m - 1)])
+        else:  # uB is a function
+            bottom_boundary_values = np.array([uB(x_linspace[i], y_linspace[0]) for i in range(1, m)])
+
+        if type(uT) == int or type(uT) == float:  # uT is a constant
+            top_boundary_values = np.array([uT for _ in range(m - 1)])
+        else:  # uT is a function
+            top_boundary_values = np.array([uT(x_linspace[i], y_linspace[-1]) for i in range(1, m)])
+
+        x_grid, y_grid = np.meshgrid(x_linspace[1:], y_linspace[1:-1])
+        # m-2 x n-1
+
+        x_coordinates = x_grid.ravel()
+        y_coordinates = y_grid.ravel()
+
+        # 경계보다 한칸 안에 있는 점들 indices
+        left_inner_boundary_indices = np.squeeze(np.where(x_coordinates == x_linspace[1]))
+        bottom_inner_boundary_indices = np.squeeze(np.where(y_coordinates == y_linspace[1]))
+        top_inner_boundary_indices = np.squeeze(np.where(y_coordinates == y_linspace[n - 2]))
+
+        # un 계산할 경계 indices
+        right_boundary_indices = np.squeeze(np.where(x_coordinates == x_linspace[-1]))
+
+        # f(x,y) 넣어줌
+        b_vec = np.array([pow(h, 2) * poisson_func(x_coordinates[i], y_coordinates[i]) for i in range(vec_len)])
+
+        # boundary conditions 넣어줌
+        for j, left_index in enumerate(left_inner_boundary_indices):
+            b_vec[left_index] -= left_boundary_values[j]
+        for i, bottom_index in enumerate(bottom_inner_boundary_indices):
+            b_vec[bottom_index] -= bottom_boundary_values[i]
+        for i, top_index in enumerate(top_inner_boundary_indices):
+            b_vec[top_index] -= top_boundary_values[i]
+
+        # 경계에서 근사치 빼줌
+        for right_index in right_boundary_indices:
+            b_vec[right_index] -= 2 * h * un(y_coordinates[right_index])
+
+        D = -4 * np.identity(m - 1) + np.eye(m - 1, k=-1) + np.eye(m - 1, k=1)
+        D[-1][-2] += 1
+        I = np.identity(m - 1)
+
+        A = np.block([[D if i == j else I for i in range(n - 2)] for j in range(n - 2)])
+        # A * u_vec = b_vec
+
+        u_vec = Iteration.GaussSeidelMethod(coeff_mat=A, b_vec=b_vec)
+
+        return u_vec
+
+    @staticmethod
+    def __NeumannWithAxisY(xrange, yrange, h, un, poisson_func, uL=None, uR=None, uB=None, uT=None):
+        # uT가 없다는 가정하에 작성
+        # un is a function of x
+
+        from Iteration import Iteration
+        x_linspace = np.arange(xrange[0], xrange[1] + h, h)
+        y_linspace = np.arange(yrange[0], yrange[1] + h, h)
+
+        m = len(x_linspace)
+        n = len(y_linspace)
+
+        vec_len = (m-2) * (n-1)
+
+        if type(uL) == int or float:  # uL is a constant
+            left_boundary_values = np.array([uL for _ in range(n-1)])
+        else:  # uL is a function
+            left_boundary_values = np.array([uL(x_linspace[0], y_linspace[j]) for j in range(1, n)])
+
+        if type(uR) == int or type(uR) == float:  # uR is a constant
+            right_boundary_values = np.array([uR for _ in range(n-1)])
+        else:  # uR is a function
+            right_boundary_values = np.array([uR(x_linspace[-1], y_linspace[j]) for j in range(1, n)])
+
+        if type(uB) == int or type(uB) == float:  # uB is a constant
+            bottom_boundary_values = np.array([uB for _ in range(m-2)])
+        else:  # uB is a function
+            bottom_boundary_values = np.array([uB(x_linspace[i], y_linspace[0]) for i in range(1, m-1)])
+
+        x_grid, y_grid = np.meshgrid(x_linspace[1:-1], y_linspace[1:])
+        # m-2 x n-1
+
+        x_coordinates = x_grid.ravel()
+        y_coordinates = y_grid.ravel()
+
+        # 경계보다 한칸 안에 있는 점들 indices
+        left_inner_boundary_indices = np.squeeze(np.where(x_coordinates == x_linspace[1]))
+        right_inner_boundary_indices = np.squeeze(np.where(x_coordinates == x_linspace[m - 2]))
+        bottom_inner_boundary_indices = np.squeeze(np.where(y_coordinates == y_linspace[1]))
+
+        # un 계산할 경계 indices
+        top_boundary_indices = np.squeeze(np.where(y_coordinates == y_linspace[-1]))
+
+        # f(x,y) 넣어줌
+        b_vec = np.array([pow(h, 2) * poisson_func(x_coordinates[i], y_coordinates[i]) for i in range(vec_len)])
+
+        # boundary conditions 넣어줌
+        for j, left_index in enumerate(left_inner_boundary_indices):
+            b_vec[left_index] -= left_boundary_values[j]
+        for j, right_index in enumerate(right_inner_boundary_indices):
+            b_vec[right_index] -= right_boundary_values[j]
+        for i, bottom_index in enumerate(bottom_inner_boundary_indices):
+            b_vec[bottom_index] -= bottom_boundary_values[i]
+
+        # 경계에서 근사치 빼줌
+        for top_index in top_boundary_indices:
+            b_vec[top_index] -= 2 * h * un(x_coordinates[top_index])
+
+        D = -4 * np.identity(m - 2) + np.eye(m - 2, k=-1) + np.eye(m - 2, k=1)
+        I = np.identity(m - 2)
+
+        A = np.block([[D if i == j else (2 * I if i == n-3 and j == n-2 else I) for i in range(n-1)] for j in range(n-1)])
+        # A * u_vec = b_vec
+
+        u_vec = Iteration.GaussSeidelMethod(coeff_mat=A, b_vec=b_vec)
+
+        return u_vec
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from RK import RK
 from Utils import Utils
 from Euler import Euler
 from Iteration import Iteration
+from PDE import PDE
 np.set_printoptions(precision=12)
 # print(2 * pow(np.e, -0.5*0.1) + pow(np.e, -1.5 * 0.1))
 # print(-1 * pow(np.e, -0.5*0.1) - 1.5 * pow(np.e, -1.5*0.1))
@@ -29,7 +30,7 @@ if __name__ == '__main__':
     error = y1_5th - y1_4th  # email pdf에 나와있는 에러
 
     # 결과 출력법
-    Utils.printResult(f, x0, y0, h, exact_y=1.20033467209)  # pdf에 있는 실제 y값 사용했음
+    # Utils.printResult(f, x0, y0, h, exact_y=1.20033467209)  # pdf에 있는 실제 y값 사용했음
     # Utils.printResult(f, x0, y0, h)  # exact_y를 주지 않았을 때의 출력
 
 
@@ -57,7 +58,7 @@ if __name__ == '__main__':
     exact_y_vec = [y, yprime]
 
     # 결과 출력법
-    Utils.printResultForSystem(F, xn, yn_vec, h, exact_y_vec=exact_y_vec)
+    # Utils.printResultForSystem(F, xn, yn_vec, h, exact_y_vec=exact_y_vec)
     # Utils.printResultForSystem(F, xn, yn_vec, h)  # exact_y_vec을 주지 않았을 때의 출력
 
 
@@ -65,7 +66,7 @@ if __name__ == '__main__':
     # target error보다 작은 에러를 얻기 위한 h구하는 함수. default stride = 0.05 (근데 많이 안해봄)
     h_target = Utils.get_h_for_target_error(exact_y=1.20033467209, method=RK.RKFifth,
                                                target_error=3.04e-9, f=f, xn=x0, yn=y0, stride=0.1)
-    print(f"h target: {h_target}")
+    # print(f"h target: {h_target}")
 
 
     # -----------------------------------------------------------------------------------------
@@ -74,16 +75,69 @@ if __name__ == '__main__':
     # input 형식(matrix A, vector b) -> Ax = b 근사
     A = np.array([[-4, 1, 1, 0],
                   [1, -4, 0, 1],
-                  [1, 0, -4, 1],
-                  [0, 1, 1, -4]])
+                  [2, 0, -4, 1],
+                  [0, 2, 1, -4]])
 
-    b_vec = np.array([-200, -200, -100, -100])
+    b_vec = np.array([0.75, 1.125, -1.5, -6])
 
     # 사용법
     x_GS_vec = Iteration.GaussSeidelMethod(A, b_vec)
     x_J_vec = Iteration.JacobiMethod(A, b_vec)
 
     # 결과 출력법
-    Utils.printResultOfIteration(coeff_mat=A, b_vec=b_vec)
+    # Utils.printResultOfIteration(coeff_mat=A, b_vec=b_vec)
 
     # TODO n번째 yn구하기, vector input validation(list -> np.ndarray)
+
+    # -----------------------------------------------------------------------------------------
+    # PDE(elliptic example)
+
+    # input 형식:
+    # (xrange, yrange, h, uL=None, uR=None, uB=None, uT=None, f=None,
+    #                     un_axis=None, un=None,  # for mixed boundary condition
+    #                     boundary_type='Dirichlet')
+
+    # boundary_type: 'dirichlet'(default) or ['mixed' or 'neumann']. 대소문자 관계 x, mixed == neumann
+    # uL, uR, uB, uT: 함수( of x, y)이거나 상수
+    # f: 기본값 None(Laplace), f(x,y) 넣어주면 포아송 방정식
+    # un_axis: un에서 n축, un: x축이면 function of y, y축이면 function of x
+
+    # ex1 laplace, dirichlet
+    Laplace_Dirichlet = PDE.PoissonEquation(xrange=(0, 12), yrange=(0, 12), h=4,
+                                            uL=0, uR=100, uB=100, uT=0)
+    print("Laplace and Dirchlet:")
+    print(Laplace_Dirichlet)
+    print()
+
+    # ex2 poisson, neumann(y-axis)
+    def f(x, y):
+        return 12 * x * y
+    def uR(x, y):
+        return 3 * pow(y, 3)
+    def un(x):
+        return 6 * x
+
+    Poisson_Neumann = PDE.PoissonEquation(xrange=(0, 1.5), yrange=(0, 1.0), h=0.5,
+                                          uL=0, uR=uR, uB=0, f=f,
+                                          un_axis='y', un=un,
+                                          boundary_type='mixed')
+    print("Poisson and Neumann(y):")
+    print(Poisson_Neumann)
+    print()
+
+
+    # ex2 poisson, neumann(x-axis)
+    def f(x, y):
+        return pow(x, 2) + pow(y, 2)
+    def uT(x, y):
+        return 9 * pow(x, 2)
+    def un(y):
+        return 6 * pow(y, 2)
+
+    Poisson_Neumann = PDE.PoissonEquation(xrange=(0, 3), yrange=(0, 3), h=1,
+                                          uL=0, uB=0, uT=uT, f=f,
+                                          un_axis='x', un=un,
+                                          boundary_type='mixed')
+    print("Poisson and Neumann(x):")
+    print(Poisson_Neumann)
+    print()
